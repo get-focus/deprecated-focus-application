2efa732627124ac952dd982d2d20cf0519b26b49
Pierre Besson <pierre.besson7@gmail.com>
2016-09-05 18:25:29 +0200

    [header] Add expanded and unexpanded version.

---
+++ src/header/header-actions.js
@@ -0,0 +1,33 @@
+export const TRIGGER_POSITION_HEADER = 'Header: TRIGGER_POSITION_HEADER';
+export const EXPAND_HEADER = 'Header: EXPAND_HEADER';
+export const UNEXPAND_HEADER = 'Header: UNEXPAND_HEADER';
+export const INJECT_BAR_CONTENT_LEFT_HEADER = 'Header: INJECT_BAR_CONTENT_LEFT_HEADER';
+export const INJECT_BAR_CONTENT_SUMMARY_HEADER = 'Header: INJECT_BAR_CONTENT_SUMMARY_HEADER';
+export const INJECT_BAR_CONTENT_RIGHT_HEADER = 'Header: INJECT_BAR_CONTENT_RIGHT_HEADER';
+export const INJECT_BAR_CONTENT_EXPANDED_HEADER = 'Header: INJECT_BAR_CONTENT_EXPANDED_HEADER';
+export const DEFAULT_TRIGGER_POSITION = 60;
+export function expandHeader(){
+  return {type: EXPAND_HEADER};
+}
+export function unExpandHeader(){
+  return {type: UNEXPAND_HEADER};
+}
+export function triggerPosition(value = DEFAULT_TRIGGER_POSITION){
+  return {type: TRIGGER_POSITION_HEADER, value};
+}
+
+export function injectBarContentLeftHeader(Component){
+  return {type: INJECT_BAR_CONTENT_LEFT_HEADER, Component};
+}
+
+export function injectBarContentRightHeader(Component){
+  return {type: INJECT_BAR_CONTENT_RIGHT_HEADER, Component};
+}
+
+export function injectBarContentSummaryHeader(Component){
+  return {type: INJECT_BAR_CONTENT_SUMMARY_HEADER, Component};
+}
+
+export function injectBarContentExpandedHeader(Component){
+  return {type: INJECT_BAR_CONTENT_EXPANDED_HEADER, Component};
+}
