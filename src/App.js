acf926581760995021d8da040f5be89ff8978469
Pierre Besson <pierre.besson7@gmail.com>
2016-09-05 18:25:52 +0200

    [app] Add example of header.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './layout';
 import fetchReducer, {totalsSelector} from './fetch/reducer';
 import LoadingBarComponent from './fetch/component';
 import {createStore, combineReducers} from 'redux';
+import ScrollTrigger from './layout/scroll-trigger';
 import {Provider, connect} from 'react-redux';
 import createfocusFetchProxy from './fetch/fetch-proxy';
 import messageReducer from './messages/reducer';
@@ -13,14 +14,15 @@ import MessageCenter from './messages/message-center';
 import './messages/message-center.css';
 import './messages/reducer';
 import AppHeader from './header/header-component';
-import headerReducer, {headerSelector} from './header/header-reducer';
+import headerReducer, {headerSelector, headerIsExpandedSelector} from './header/header-reducer';
 import {
   expandHeader,
   unExpandHeader,
   injectBarContentLeftHeader,
   injectBarContentRightHeader,
   injectBarContentSummaryHeader,
-  injectBarContentExpandedHeader
+  injectBarContentExpandedHeader,
+  triggerPosition
 } from './header/header-actions'
 
 const ConnectedHeader = connect(headerSelector)(AppHeader)
@@ -35,6 +37,15 @@ const ConnectedMessageCenter = connect(
 )(MessageCenter);
 
 const log = d => console.log({logger: d})
+const ConnectedScrollTrigger = connect(
+  headerIsExpandedSelector,
+  d => ({
+    expandHeader: () => d(expandHeader()),
+    unExpandHeader: () => d(unExpandHeader())
+  }))(ScrollTrigger);
+
+
+const Debug = connect(s => ({redux: s}))(props => <pre><code>{JSON.stringify(props.redux, null, 4)}</code></pre>)
 
 class App extends PureComponent {
   componentWillMount(){
@@ -56,21 +67,32 @@ class App extends PureComponent {
       injectBarContentLeftHeader: () =>dispatch(injectBarContentLeftHeader(p => <div>'injectBarContentLeftHeader'</div>)),
       injectBarContentRightHeader: () =>dispatch(injectBarContentRightHeader(p => <div>'injectBarContentRightHeader'</div>)),
       injectBarContentSummaryHeader:() => dispatch(injectBarContentSummaryHeader(p => <div>'injectBarContentSummaryHeader'</div>)),
-      injectBarContentExpandedHeader: () =>dispatch(injectBarContentExpandedHeader(p => <div>'injectBarContentExpandedHeader'</div>))
+      injectBarContentExpandedHeader: () =>dispatch(injectBarContentExpandedHeader(p => <div>'injectBarContentExpandedHeader'</div>)),
+      triggerPosition: () => dispatch(triggerPosition(150)),
+      clearTrigger: () => dispatch(triggerPosition())
     }
     return <Provider store={store}>
-      <Layout AppHeader={ConnectedHeader} MessageCenter={ConnectedMessageCenter}>
-        <div style={{display: 'flex', justifyContent:'space-around'}}>
-          <button onClick={() => dispatch({type: 'PUSH_MESSAGE', message:{id: `msg_${msgId++}`, type: 'info'}})}>Push</button>
-          <button onClick={actions.expandHeader}>expandHeader</button>
-          <button onClick={actions.unExpandHeader}>unExpandHeader</button>
-          <button onClick={actions.injectBarContentLeftHeader}>injectBarContentLeftHeader</button>
-          <button onClick={actions.injectBarContentRightHeader}>injectBarContentRightHeader</button>
-          <button onClick={actions.injectBarContentSummaryHeader}>injectBarContentSummaryHeader</button>
-          <button onClick={actions.injectBarContentExpandedHeader}>injectBarContentExpandedHeader</button>
-        </div>
-        <ConnectedLoadingBar/>
-      </Layout>
+      <ConnectedScrollTrigger>
+        <Layout AppHeader={ConnectedHeader} MessageCenter={ConnectedMessageCenter}>
+          <div style={{display: 'flex', justifyContent:'space-around'}}>
+            <button onClick={() => dispatch({type: 'PUSH_MESSAGE', message:{id: `msg_${msgId++}`, type: 'info'}})}>Push</button>
+            <button onClick={actions.expandHeader}>expandHeader</button>
+            <button onClick={actions.unExpandHeader}>unExpandHeader</button>
+            <button onClick={actions.injectBarContentLeftHeader}>injectBarContentLeftHeader</button>
+            <button onClick={actions.injectBarContentRightHeader}>injectBarContentRightHeader</button>
+            <button onClick={actions.injectBarContentSummaryHeader}>injectBarContentSummaryHeader</button>
+            <button onClick={actions.injectBarContentExpandedHeader}>injectBarContentExpandedHeader</button>
+            <button onClick={actions.triggerPosition}>triggerPosition</button>
+            <button onClick={actions.clearTrigger}>cleartrigger</button>
+          </div>
+          <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', height: 300, width:'100%'}}>Hello</div>
+          <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', height: 300, width:'100%'}}>Hello</div>
+          <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', height: 300, width:'100%'}}>Hello</div>
+          <div style={{display: 'flex', justifyContent:'center', alignItems: 'center', height: 300, width:'100%'}}>Hello</div>
+          <ConnectedLoadingBar/>
+        </Layout>
+        <Debug />
+      </ConnectedScrollTrigger>
     </Provider>
     ;
   }
