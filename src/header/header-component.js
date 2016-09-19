a36e79c931228444fb3954bf1c9a1076cfb7a4a0
Thomas <tgallon@gmail.com>
2016-09-19 20:38:29 +0200

    restyle header

--- src/header/header-component.js
+++ src/header/header-component.js
@@ -1,65 +1,69 @@
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 
 const fixedStyle = {position: 'fixed', width: '100%', top: '0px', zIndex: 1000};
 
 const Dropdown = props => <div>{JSON.stringify(props, null, 4)}</div>
 
 function ContentActions({primary, secondary}){
-  return (
-    <div data-focus='content-actions'>
-        {primary.map((primary) => {
-            if(Array.isArray(primary.action)) {
-                return <Dropdown iconProps={{name: primary.icon}} operationList={primary.action} shape='fab'/>;
-            } else {
-                return (
-                    <Button handleOnClick={primary.action} icon={primary.icon} label={primary.label} shape='fab' style={{className: primary.className}} type='button'/>
-                );
-            }
-        })}
-        <Dropdown iconProps={{name: 'more_vert'}} operationList={secondary} shape='fab'/>
-    </div>
-  );
+    return (
+        <div data-focus='content-actions'>
+            {primary.map((primary) => {
+                if(Array.isArray(primary.action)) {
+                    return <Dropdown iconProps={{name: primary.icon}} operationList={primary.action} shape='fab'/>;
+                } else {
+                    return (
+                        <Button handleOnClick={primary.action} icon={primary.icon} label={primary.label} shape='fab' style={{className: primary.className}} type='button'/>
+                    );
+                }
+            })}
+            <Dropdown iconProps={{name: 'more_vert'}} operationList={secondary} shape='fab'/>
+        </div>
+    );
 }
+
 
 function HeaderComponent({
-  isExpanded,
-  BarContentLeft,
-  BarContentSummary,
-  BarContentRight,
-  BarContentExpanded,
-  ContentActionsComponent,
-  triggerPosition,
-  actions
-}){
-    return (<header data-focus='header-bar' data-is-expanded={isExpanded}>
-        <nav data-focus='bar'>
-          <BarContentLeft />
-          {!isExpanded && <BarContentSummary />}
-          <BarContentRight/>
-        </nav>
-        {isExpanded ?  <BarContentExpanded/> :  <div style={{width: '100%', height: `${triggerPosition}px`}}></div>}
-        {/* Actions primary and secondary*/}
-        {actions && <ContentActionsComponent primary={actions.primary} secondary={actions.secondary} />}
-    </header>)
-}
-const fakeStyle = {backgroundColor: 'blue', color: 'white'};
-const fakeComponentCreator = name => function(props){return <pre style={fakeStyle}><h2>{name}</h2><code>{JSON.stringify(props, null, 4)}</code></pre>}
-HeaderComponent.defaultProps = {
-  isExpanded: true,
-  BarContentLeft: fakeComponentCreator('BarContentLeft'),
-  BarContentSummary: fakeComponentCreator('BarContentSummary'),
-  BarContentRight: fakeComponentCreator('BarContentRight'),
-  BarContentExpanded: fakeComponentCreator('BarContentExpanded'),
-  actions: {primary: [], secondary: []},
-  ContentActionsComponent: ContentActions
+    isExpanded,
+    BarContentLeft,
+    BarContentSummary,
+    BarContentRight,
+    BarContentExpanded,
+    ContentActionsComponent,
+    triggerPosition,
+    actions
+}) {
+    return (
+        <header data-focus='header' data-is-expanded={isExpanded}>
+            <nav data-focus='header-bar'>
+                <div data-focus='header-bar-left'><BarContentLeft /></div>
+                {!isExpanded && <div data-focus='header-bar-summary'><BarContentSummary /></div>}
+                <div data-focus='header-bar-right'><BarContentRight/></div>
+            </nav>
+            <div data-focus='header-bar-expanded'><BarContentExpanded/></div>
+            {/* Actions primary and secondary*/}
+            {actions && <div data-focus='header-bar-actions'><ContentActionsComponent primary={actions.primary} secondary={actions.secondary} /></div>}
+        </header>
+    );
 }
 
-HeaderComponent.propTypes = {
-  isExpanded: PropTypes.bool,
-  BarContentLeft: PropTypes.func,
-  BarContentSummary: PropTypes.func,
-  BarContentRight: PropTypes.func,
-  BarContentExpanded: PropTypes.func
-}
+const fakeComponentCreator = name => function(props){return <span>{name} - {JSON.stringify(props)}</span>}
 
-export default HeaderComponent
+    HeaderComponent.defaultProps = {
+        isExpanded: true,
+        BarContentLeft: fakeComponentCreator('BarContentLeft'),
+        BarContentSummary: fakeComponentCreator('BarContentSummary'),
+        BarContentRight: fakeComponentCreator('BarContentRight'),
+        BarContentExpanded: fakeComponentCreator('BarContentExpanded'),
+        actions: {primary: [], secondary: []},
+        ContentActionsComponent: ContentActions
+    }
+
+    HeaderComponent.propTypes = {
+        isExpanded: PropTypes.bool,
+        BarContentLeft: PropTypes.func,
+        BarContentSummary: PropTypes.func,
+        BarContentRight: PropTypes.func,
+        BarContentExpanded: PropTypes.func
+    }
+
+    export default HeaderComponent
