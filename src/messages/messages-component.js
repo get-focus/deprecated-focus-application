eceae3653ead79f2485da42732d83fe0b2002a63
Thomas <tgallon@gmail.com>
2016-12-13 11:28:25 +0100

    rewrite message center in pure component

--- src/messages/messages-component.js
+++ src/messages/messages-component.js
@@ -1,17 +1,28 @@
-import React , {PureComponent, PropTypes} from 'react';
+import React, {PropTypes, PureComponent} from 'react';
 import i18next from 'i18next';
 import capitalize from 'lodash/capitalize';
 
-function Message({messageId, content, title, deleteMessage}){
-    return (
-        <div data-focus='message'>
-            <span>{messageId}</span>
-            <span>{i18next.t(title)}</span>
-            <span>{i18next.t(content)}</span>
-            <button onClick={() => deleteMessage({id: id})}>x</button>
-        </div>
-    );
+class Message extends PureComponent {
+    render() {
+        const {content, deleteMessage, messageId, title} = this.props;
+        return (
+            <div data-focus='message'>
+                <span>{messageId}</span>
+                <span>{i18next.t(title)}</span>
+                <span>{i18next.t(content)}</span>
+                <button onClick={deleteMessage}>x</button>
+            </div>
+        );
+    }
 };
+Message.displayName = 'Message';
+Message.propTypes = {
+    content: PropTypes.string.isRequired,
+    deleteMessage: PropTypes.func,
+    messageId: PropTypes.string.isRequired,
+    title: PropTypes.string
+}
+
 
 class MessageWithTtl extends PureComponent {
     componentDidMount() {
@@ -28,26 +39,36 @@ class MessageWithTtl extends PureComponent {
         return <MessageComponent {...otherMessageProps} />
     }
 };
+MessageWithTtl.displayName = 'MessageWithTtl';
+MessageWithTtl.PropTypes = {
+    ttl: PropTypes.number,
+    MessageComponent: PropTypes.func
+};
 MessageWithTtl.defaultProps = {
     ttl: 5000,
     MessageComponent: Message
 };
 
-export default function MessageCenter({MessageComponent, messages, deleteMessage, ...otherProps}){
-    return (
-        <div data-focus='message-center'>
-            {messages.map((msg) => {
-                const messageTtl = msg.ttl ? msg.ttl : otherProps[`ttl${capitalize(msg.type)}`];
-                const {id} = msg;
-                return (
-                    <MessageWithTtl key={msg.id} ttl={messageTtl} {...msg} messageId={id} deleteMessage={deleteMessage} MessageComponent={MessageComponent} />
-                );
-            })}
-        </div>
-    );
+
+export default class MessageCenter extends PureComponent {
+    render() {
+        const {deleteMessage, MessageComponent, messages, ...otherProps} = this.props;
+        return (
+            <div data-focus='message-center'>
+                {messages.map((msg) => {
+                    const messageTtl = msg.ttl ? msg.ttl : otherProps[`ttl${capitalize(msg.type)}`];
+                    const {id} = msg;
+                    return (
+                        <MessageWithTtl key={msg.id} ttl={messageTtl} {...msg} messageId={id} deleteMessage={deleteMessage} MessageComponent={MessageComponent} />
+                    );
+                })}
+            </div>
+        );
+    };
 };
 MessageCenter.propTypes = {
     deleteMessage: PropTypes.func.isRequired,
+    MessageComponent: PropTypes.func,
     messages: PropTypes.array.isRequired,
     ttlInfo: PropTypes.number,
     ttlError: PropTypes.number,
